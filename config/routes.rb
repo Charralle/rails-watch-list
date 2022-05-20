4a9bbbd428ca14a77c690243776ec32a23539b1d
Charlesalerxandre Tiago <charles-alexandre.tiago@hotmail.com>
2022-05-20 17:15:41 +0200

    Herokuuu

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :bookmarks, only: [:new, :index, :create, :update]
   end
   # Defines the root path route ("/")
+  root to: "lists#index"
   # root "articles#index"
 end
