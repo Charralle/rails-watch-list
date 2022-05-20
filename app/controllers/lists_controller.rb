98a20366359446b9db4050087648ed984bc341e2
Charlesalerxandre Tiago <charles-alexandre.tiago@hotmail.com>
2022-05-20 17:03:32 +0200

    Herokuuu

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,50 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy!
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :bookmark_ids, :movie_ids, :comment, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+
+p "Importing movies sir !"
+url = "http://tmdb.lewagon.com/movie/top_rated?"
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)["results"]
+
+movies.each do |movie|
+  title = movie["title"]
+  overview = movie["overview"]
+  rating = movie["vote_average"]
+  poster_url = movie["poster_path"]
+  Movie.create([title: title, overview: overview, rating: rating, poster_url: "https://image.tmdb.org/t/p/w500/#{poster_url}"])
+end
+
+p "You have a clean db full of movies"
